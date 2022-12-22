a9f0a9c2e82448fd1aa2409def7910ef6805ef1c
leedahye2001 <leedahye2001@naver.com>
2022-12-23 04:12:45 +0900

    css modification

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,11 +4,10 @@ class Nav extends Component {
   render() {
     return (
       <div>
+        <div className="m-4 font-black">aasdsdfsasdf</div>
         <h1>
-          <p>
-            • 글 쓰기/수정 • 글 목록/삭제 • 댓글 • UI디자인 • 관리자도구 •
-            좋아요(option) • Trackback (option) • RSS (option)
-          </p>
+          • 글 쓰기/수정 • 글 목록/삭제 • 댓글 • UI디자인 • 관리자도구 •
+          좋아요(option) • Trackback (option) • RSS (option) asdfadsfdsafas
         </h1>
       </div>
     );
