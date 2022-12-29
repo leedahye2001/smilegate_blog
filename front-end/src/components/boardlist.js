1534324c2efc2ecb621257d774f6e8c1ba32bc56
leedahye2001 <leedahye2001@naver.com>
2022-12-29 23:39:14 +0900

    db connection

--- front-end/src/components/boardlist.js
+++ front-end/src/components/boardlist.js
@@ -2,39 +2,7 @@ import { Component } from "react";
 
 class BoardList extends Component {
   render() {
-    return (
-      //   <Table striped bordered hover>
-      //     <thead>
-      //       <tr>
-      //         <th>번호</th>
-      //         <th>제목</th>
-      //         <th>작성자</th>
-      //         <th>작성일</th>
-      //       </tr>
-      //     </thead>
-      //     <tbody>
-      //       <tr>
-      //         <td>1</td>
-      //         <td>게시글1</td>
-      //         <td>artistJay</td>
-      //         <td>2022-03-19</td>
-      //       </tr>
-      //       <tr>
-      //         <td>2</td>
-      //         <td>게시글2</td>
-      //         <td>artistJay</td>
-      //         <td>2022-03-19</td>
-      //       </tr>
-      //       <tr>
-      //         <td>3</td>
-      //         <td>게시글2</td>
-      //         <td>artistJay</td>
-      //         <td>2022-03-19</td>
-      //       </tr>
-      //     </tbody>
-      //   </Table>
-      <table>asfd</table>
-    );
+    return <table></table>;
   }
 }
 
